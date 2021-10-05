5a8b16ff1328720b14f63b148cce2320c66272b6
dedicDinoSven <dinosven.dedic@hotmail.com>
2021-10-05 23:17:40 +0200

    deployment preparation

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = process.env.ATLAS_URI
+const mongoURI = process.env.MONGO_URI
 
 mongoose
 	.connect(mongoURI, {
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
-
+const path = require('path');
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 require('./db');
-const path = require('path');
 
 const app = express();
 
@@ -19,11 +18,13 @@ app.use('/question', require('./routes/question'));
 app.use('/answer', require('./routes/answer'));
 app.use('/rating', require('./routes/rating'));
 
-/* app.use(express.static(path.join(__dirname, "client", "build")))
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/client/build')));
 
-app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-}); */
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
